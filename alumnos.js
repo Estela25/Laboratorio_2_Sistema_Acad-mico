e019d4ddcbdd1a57fa154531ef02fd032f258ff2
Genesis Hernández <scarletgenesis2807@gmail.com>
2023-03-19 18:04:13 -0600

    cambios

--- alumnos.js
+++ alumnos.js
@@ -1,85 +1,129 @@
-<!DOCTYPE html><!-- Definicion de tipo de documento -->
-<html lang="en">
-<!-- Apertura de la pagina html -->
-<head>
-    <!-- Encabezado de la pagina html -->
-    <meta charset="UTF-8"><!-- Codificacion de Caracteres especiales  -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"><!-- IE -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/vue-select@3.0.0/dist/vue-select.css">
-    <title>::.. APP ACADEMICA ..::</title>
-</head>
-<body>
-    <!-- Contenido de la pagina html -->
-    <div class="container-fluid" id="app">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">::.. SISTEMA ACADEMICO ..::</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" @click="abrirCerrarFormulario('docente')" href="#">Docentes</a>
-                        <a class="nav-link" @click="abrirCerrarFormulario('alumno')" href="#">Alumnos</a>
-                        <a class="nav-link" @click="abrirCerrarFormulario('materia')" href="#">Materia</a>
-                        <a class="nav-link" @click="abrirCerrarFormulario('matricula')" href="#">Matricula</a>
-                        <a class="nav-link" @click="abrirCerrarFormulario('inscripcion')" href="#">Inscripcion</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <div id="sistemApp">
-            <docentes ref="docente" v-show="forms['docente'].mostrar"></docentes>
-            <materias ref="materia" v-show="forms['materia'].mostrar"></materias>
-            <alumnos ref="alumno" v-show="forms['alumno'].mostrar"></alumnos>
-            <matriculas ref="matricula" v-show="forms['matricula'].mostrar"></alumnos>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> NO soporta SFC con la CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
-    <script src="https://unpkg.com/vue-select@3.0.0"></script>
-    <script src="imagen.js"></script>
-    <script src="docentes.js"></script>
-    <script src="materias.js"></script>
-    <script src="alumnos.js"></script>
-    <script src="matriculas.js"></script>
-    <script src="main.js"></script>
-</body>
-
-</html>
-
-
-
-Vue.componet('alumnos', {
+Vue.component('alumnos', {
     data() {
         return {
-            db      :'',
-            buscar  :'',
+            db       : '',
+            buscar   : '',
             alumnos : [],
-            accion  : 'nuevo',
+            accion   : 'nuevo',
             alumno  : {
-                idAlumno :'',
-                codigo   :'',
-                nombre   : ''
+                idAlumno : '',
+                codigo    : '',
+                nombre    : ''
             }
         }
-    }
-},
-)
-methods:{
-    nuevoAlumno();
-    this.accion = 'nuevo';
+    },
+    methods:{
+        nuevoAlumno(){
+            this.accion = 'nuevo';
             this.alumno.idAlumno = '';
             this.alumno.codigo = '';
             this.alumno.nombre = '';
-}
-modificarAlumno(alumno){
-    this.accion = 'modificar';
-    this.alumno = alumno;
-}
-    
+        },
+        modificarAlumno(alumno){
+            this.accion = 'modificar';
+            this.alumno = alumno;
+        },
+        guardarAlumno(){
+            if( this.alumno.nombre=='' || 
+                this.alumno.codigo=='' ){
+                console.log( 'Por favor ingrese los datos correspondientes' );
+                return;
+            }
+            let store = abrirStore("tblalumnos", 'readwrite');
+            if( this.accion==='nuevo' ){
+                this.alumno.idAlumno = new Date().getTime().toString(16);//las cantidad milisegundos y lo convierte en hexadecimal   
+            }
+            let query = store.put( JSON.parse( JSON.stringify(this.alumno) ));
+            query.onsuccess = resp=>{
+                this.nuevoAlumno();
+                this.listar();
+            };
+            query.onerror = err=>{
+                console.error('ERROR al guardar alumno', err);
+            };
+        },
+        eliminarAlumno(alumno){
+            if( confirm(`Esta seguro de eliminar el alumno ${alumno.nombre}?`) ){
+                let store = abrirStore('tblalumnos', 'readwrite'),
+                    req = store.delete(alumno.idAlumno);
+                req.onsuccess = res=>{
+                    this.listar();
+                };
+                req.onerror = err=>{
+                    console.error('ERROR al guardar alumno');
+                };
+            }
+        },
+        listar(){
+            let store = abrirStore('tblalumnos', 'readonly'),
+                data = store.getAll();
+            data.onsuccess = resp=>{
+                this.alumnos = data.result
+                    .filter(alumno=>alumno.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>-1 || 
+                        alumno.codigo.indexOf(this.buscar)>-1);
+            };
+        },
+    },
+    template : `
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="card border-primary">
+                        <div class="card-header bg-primary text-white">REGISTRO DE ALUMNOS</div>
+                        <div class="card-body">
+                            <form id="frmAlumno" @submit.prevent="guardarAlumno" @reset.prevent="nuevoAlumno()">
+                                <div class="row p-1">
+                                    <div class="col-3 col-md-2">CODIGO:</div>
+                                    <div class="col-9 col-md-3">
+                                        <input required pattern="[US|SM]{2}[SI|LE|LI]{2}[0-9]{6}" class="form-control" type="text" v-model="alumno.codigo">
+                                    </div>
+                                </div>
+                                <div class="row p-1">
+                                    <div class="col-3 col-md-2">NOMBRE:</div>
+                                    <div class="col-9 col-md-6">
+                                        <input required pattern="[a-zA-Z ]{3,65}" class="form-control" type="text" v-model="alumno.nombre">
+                                    </div>
+                                </div>
+                                <div class="row p-1">
+                                    <div class="col col-md-6">
+                                        <input class="btn btn-success" type="submit" value="Guardar Datos">
+                                    </div>
+                                    <div class="col col-md-6">
+                                        <input class="btn btn-warning" type="reset" value="Nuevo Registro">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="card text-bg-light">
+                        <div class="card-header">CONSULTA DE ALUMNOS</div>
+                        <div class="card-body">
+                            <form>
+                                <table class="table table-dark table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>BUSCAR:</th>
+                                            <th colspan="2"><input type="text" @keyup="listar()" v-model="buscar" 
+                                                class="form-control" placeholder="Busar por nombre" ></th>
+                                        </tr>
+                                        <tr>
+                                            <th>CODIGO</th>
+                                            <th colspan="2">NOMBRE</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="alumno in alumnos" @click='modificarAlumno(alumno)' :key="alumno.idAlumno">
+                                            <td>{{alumno.codigo}}</td>
+                                            <td>{{alumno.nombre}}</td>
+                                            <td><button @click.prevent="eliminarAlumno(alumno)" class="btn btn-danger">Eliminar</button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+});
